8f24e277ed9783c50b2367d5d43535f835282d0f
MrHonor <danielamezquita5101@gmail.com>
2024-09-13 22:55:49 -0500

    Preview doesnt work

---
+++ photo-gallery/src/main.ts
@@ -0,0 +1,5 @@
+import { defineCustomElements } from '@ionic/pwa-elements/loader';
+
+// Call the element loader before the bootstrapModule/bootstrapApplication call
+defineCustomElements(window);
+
